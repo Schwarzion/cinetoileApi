acdd63cb523bd83987408c096fa1b26124dc1c1f
Thomas Vionnet <th.vionnet@gmail.com>
2021-01-06 15:52:23 +0100

    (feat): add cors
    Add CORS globally for all origins

--- src/main/java/com/cinetoile/SpringAPI/CinetoileApplication.java
+++ src/main/java/com/cinetoile/SpringAPI/CinetoileApplication.java
@@ -2,12 +2,26 @@ package com.cinetoile.SpringAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CinetoileApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CinetoileApplication.class, args);
+	}
+
+	//Enable Cors for all origins, for GET and POST methods
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST");
+			}
+		};
 	}
 
 }
