7b1563ccd3e8c9eff8434637ff8d82b69f801679
Laurene Flamey <laurene.flamey@live.fr>
2021-02-05 11:26:24 +0100

    Implement UserDTO and Spring Security.

---
+++ src/main/java/com/cinetoile/SpringAPI/Dto/UserDTO.java
@@ -0,0 +1,79 @@
+package com.cinetoile.SpringAPI.Dto;
+
+import java.sql.Timestamp;
+
+public class UserDTO {
+
+    private String firstname;
+    private String lastname;
+    private String city;
+    private String postalCode;
+    private Timestamp birthdate;
+    private String phone;
+    private String mail;
+    private String password;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Timestamp getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Timestamp birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
