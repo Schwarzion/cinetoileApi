2c3e123112c9fdfc9b7a927c256584a822aa3ac0
Lehouck Victor <victor.lehouck@gmail.com>
2021-04-29 10:54:27 +0000

    (feat): Adding Session / Reservation handling

--- src/main/java/com/cinetoile/SpringAPI/repository/ReservationRepository.java
+++ src/main/java/com/cinetoile/SpringAPI/repository/ReservationRepository.java
@@ -1,10 +1,29 @@
 package com.cinetoile.SpringAPI.repository;
 
 import com.cinetoile.SpringAPI.models.ReservationEntity;
+import com.cinetoile.SpringAPI.models.SessionEntity;
+import com.cinetoile.SpringAPI.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<ReservationEntity, Integer> {
+    List<ReservationEntity> findAllByUserId(UserEntity user);
 
+    @Query("select a from ReservationEntity a where a.userId.id = :userId and a.sessionId.time < :sessionTime")
+    List<ReservationEntity> findAllPassedByUser(
+            @Param("sessionTime") Date sessionTime,
+            @Param("userId") Integer id);
+
+    @Query("select a from ReservationEntity a where a.userId.id = :userId and a.sessionId.time >= :sessionTime")
+    List<ReservationEntity> findAllIncomingByUser(
+            @Param("sessionTime") Date sessionTime,
+            @Param("userId") Integer id);
+
+    List<ReservationEntity> findAllBySessionIdOrderByStatus(SessionEntity session);
 }
--- src/main/java/com/cinetoile/SpringAPI/services/ReservationService.java
+++ src/main/java/com/cinetoile/SpringAPI/services/ReservationService.java
@@ -3,7 +3,7 @@ package com.cinetoile.SpringAPI.services;
 import com.cinetoile.SpringAPI.NotFoundException;
 import com.cinetoile.SpringAPI.dto.dtoIn.ReservationDTOIn;
 import com.cinetoile.SpringAPI.dto.dtoOut.ReservationDTOOut;
-import com.cinetoile.SpringAPI.dto.dtoOut.SessionDTOOut;
+import com.cinetoile.SpringAPI.dto.dtoOut.ReservationUserListDTOOut;
 import com.cinetoile.SpringAPI.models.*;
 import com.cinetoile.SpringAPI.repository.PricingRepository;
 import com.cinetoile.SpringAPI.repository.ReservationRepository;
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,16 +32,37 @@ public class ReservationService {
         this.priceRepository = priceRepository;
     }
 
+    public ReservationDTOOut find(Integer id) {
+        ReservationEntity reservation = repository.findById(id).orElseThrow(() -> new NotFoundException("reservation ", id.toString()));
+        return convertToReservationDto(reservation);
+    }
+
     public List<ReservationDTOOut> findAll() {
         List<ReservationEntity> list = repository.findAll();
         return list.stream()
-                .map(reservation -> new ReservationDTOOut(reservation.getId(), reservation.getStatus(), reservation.getUserId().getId(), reservation.getSessionId().getId(), reservation.getPriceId().getId()))
+                .map(this::convertToReservationDto)
                 .collect(Collectors.toList());
     }
 
-    public ReservationDTOOut find(Integer id) {
-        ReservationEntity reservation = repository.findById(id).orElseThrow(() -> new NotFoundException("reservation ", id.toString()));
-        return new ReservationDTOOut(reservation.getId(), reservation.getStatus(), reservation.getUserId().getId(), reservation.getSessionId().getId(), reservation.getPriceId().getId());
+    public List<ReservationUserListDTOOut> findAllByUser(Integer id) {
+        return convertToReservationUserListDTOOut(repository.findAllByUserId(userRepository.findById(id).orElseThrow(() -> new NotFoundException("user ", id.toString()))));
+    }
+
+    public List<ReservationDTOOut> findAllBySession(Integer id) {
+        List<ReservationEntity> list = repository.findAllBySessionIdOrderByStatus(sessionRepository.findById(id).orElseThrow(() -> new NotFoundException("session ", id.toString())));
+        return list.stream()
+                .map(this::convertToReservationDto)
+                .collect(Collectors.toList());
+    }
+
+    public List<ReservationUserListDTOOut> findAllPassedByUser(Integer id) {
+        List<ReservationEntity> list = repository.findAllPassedByUser(new Timestamp(new Date().getTime()), userRepository.findById(id).orElseThrow(() -> new NotFoundException("user ", id.toString())).getId());
+        return convertToReservationUserListDTOOut(list);
+    }
+
+    public List<ReservationUserListDTOOut> findAllIncomingByUser(Integer id) {
+        List<ReservationEntity> list = repository.findAllIncomingByUser(new Timestamp(new Date().getTime()), id);
+        return convertToReservationUserListDTOOut(list);
     }
 
     public ReservationDTOOut add(ReservationDTOIn newReservation) {
@@ -49,15 +71,31 @@ public class ReservationService {
         PricingEntity price = this.priceRepository.findById(newReservation.getPriceId()).orElseThrow(() -> new NotFoundException("price ", newReservation.getPriceId().toString()));
         ReservationEntity reservation = new ReservationEntity(user, session, price);
         ReservationEntity result = repository.save(reservation);
-        return new ReservationDTOOut(result.getId(), result.getStatus(), result.getUserId().getId(), result.getSessionId().getId(), result.getPriceId().getId());
+        return convertToReservationDto(result);
     }
 
     public ReservationDTOOut updateStatus(Integer id, Integer status) {
         return repository.findById(id).map(reservation -> {
             reservation.setStatus(status);
             reservation = repository.save(reservation);
-            return new ReservationDTOOut(reservation.getId(), reservation.getStatus(), reservation.getUserId().getId(), reservation.getSessionId().getId(), reservation.getPriceId().getId());
+            return convertToReservationDto(reservation);
         }).orElseThrow(() -> new NotFoundException("session ", id.toString()));
+    }
+
+    private ReservationDTOOut convertToReservationDto(ReservationEntity reservation) {
+        return new ReservationDTOOut(reservation.getId(), reservation.getStatus(), reservation.getUserId().getId(), reservation.getSessionId().getId(), reservation.getPriceId().getId());
+    }
+
+    private List<ReservationUserListDTOOut> convertToReservationUserListDTOOut(List<ReservationEntity> list) {
+        return list.stream()
+                .map(reservation -> new ReservationUserListDTOOut(reservation.getId(),
+                        reservation.getStatus(),
+                        reservation.getPriceId().getPrice(),
+                        reservation.getPriceId().getName(),
+                        reservation.getSessionId().getRoomId().getTheater().getName(),
+                        reservation.getSessionId().getMovieId().getName(),
+                        reservation.getSessionId().getTime()))
+                .collect(Collectors.toList());
     }
 }
 
