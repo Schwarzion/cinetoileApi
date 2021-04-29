9b895bd0d0cb4a4731ed41c3cf0a7822c1381b72
Thomas <th.vionnet@gmail.com>
2021-04-29 12:47:21 +0000

    FEAT(theater_movie): Add Controller, Service, DTOIn, DTOOut, Repository, TU

--- src/main/java/com/cinetoile/SpringAPI/models/MovieEntity.java
+++ src/main/java/com/cinetoile/SpringAPI/models/MovieEntity.java
@@ -3,6 +3,7 @@ package com.cinetoile.SpringAPI.models;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
--- src/main/java/com/cinetoile/SpringAPI/models/TheaterMovieEntity.java
+++ src/main/java/com/cinetoile/SpringAPI/models/TheaterMovieEntity.java
@@ -1,8 +1,8 @@
 package com.cinetoile.SpringAPI.models;
 
 import lombok.Data;
-
 import javax.persistence.*;
+import java.util.Date;
 import java.sql.Timestamp;
 
 @Entity
@@ -11,6 +11,7 @@ import java.sql.Timestamp;
 public class TheaterMovieEntity {
     @Id
     @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Basic
@@ -23,19 +24,19 @@ public class TheaterMovieEntity {
 
     @Basic
     @Column(name = "status", nullable = false)
-    private int status;
+    private Boolean status;
 
     @Basic
     @Column(name = "theaterFav", nullable = false)
-    private int theaterFav;
+    private Boolean theaterFav;
 
-    @Basic
-    @Column(name = "movieId", nullable = false)
-    private int movieId;
+    @ManyToOne(targetEntity = MovieEntity.class, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "movieId", nullable = false, referencedColumnName = "id")
+    private MovieEntity movie;
 
-    @Basic
-    @Column(name = "theaterId", nullable = false)
-    private int theaterId;
+    @ManyToOne(targetEntity = TheaterEntity.class, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "theaterId", nullable = false, referencedColumnName = "id")
+    private TheaterEntity theater;
 
     @Basic
     @Column(name = "createdAt", nullable = false)
@@ -44,4 +45,17 @@ public class TheaterMovieEntity {
     @Basic
     @Column(name = "updatedAt", nullable = false)
     private Timestamp updatedAt;
+
+    public TheaterMovieEntity() {};
+
+    public TheaterMovieEntity(Timestamp startDate, Timestamp endDate, Boolean status, Boolean theaterFav, MovieEntity movie, TheaterEntity theater){
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+        this.theaterFav = theaterFav;
+        this.movie = movie;
+        this.theater = theater;
+        this.createdAt = new Timestamp(new Date().getTime());
+        this.updatedAt = new Timestamp(new Date().getTime());
+    }
 }
