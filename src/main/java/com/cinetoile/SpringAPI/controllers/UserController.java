1003870bab39bc6c316bab02ba9f3049533b7c55
Laurene Flamey <laurene.flamey@live.fr>
2021-05-25 18:02:07 +0200

    Add authentication.

--- src/main/java/com/cinetoile/SpringAPI/controllers/UserController.java
+++ src/main/java/com/cinetoile/SpringAPI/controllers/UserController.java
@@ -1,46 +1,68 @@
 package com.cinetoile.SpringAPI.controllers;
 
+import com.cinetoile.SpringAPI.dto.dtoOut.UserDTOOut;
 import com.cinetoile.SpringAPI.models.UserEntity;
+import com.cinetoile.SpringAPI.dto.dtoIn.LoginDTOIn;
+import com.cinetoile.SpringAPI.dto.dtoIn.SignupDTOIn;
+import com.cinetoile.SpringAPI.repository.UserRepository;
 import com.cinetoile.SpringAPI.services.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
-// All data returned is written into the response instead of rendering template
 @RestController
 public class UserController {
 
-    private final UserService userService;
+    UserService userService;
 
     @Autowired
-    UserController(UserService userService) {
+    UserRepository userRepository;
+
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
+    @GetMapping("/profile")
+    UserDTOOut getCurrentProfile() {
+        return this.userService.findCurrentUser();
+    }
+
+    @PostMapping("/signin")
+    ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginDTOIn loginRequest) {
+        return this.userService.signin(loginRequest);
+    }
+
+    @PostMapping("/signup")
+    ResponseEntity<?> registerUser(@Valid @RequestBody SignupDTOIn signUpRequest) {
+        return this.userService.signup(signUpRequest);
+    }
+
     @GetMapping("/users")
-    List<UserEntity> all() {
+    List<UserDTOOut> all() {
         return this.userService.findAll();
     }
 
     @GetMapping("/user/{id}")
-    UserEntity one(@PathVariable Integer id) {
-        return this.userService.findById(id);
-    }
-
-    @PostMapping("/user")
-    UserEntity add(@RequestBody UserEntity newUser) {
-        return this.userService.add(newUser);
+    UserDTOOut one(@PathVariable Integer id) {
+        return this.userService.findDto(id);
     }
 
     @PutMapping("/user/{id}")
-    UserEntity update(@RequestBody UserEntity newUser, @PathVariable Integer id) {
+    UserDTOOut update(@RequestBody UserEntity newUser, @PathVariable Integer id) {
         return this.userService.update(newUser, id);
+    }
+
+    @PutMapping("user/{id}/{status}")
+    UserDTOOut updateStatus(@PathVariable Integer id, @PathVariable Integer status) {
+        return this.userService.updateStatus(id, status);
     }
 
     @DeleteMapping("/user/{id}")
     void delete(@PathVariable Integer id) {
         this.userService.delete(id);
     }
-
 }
